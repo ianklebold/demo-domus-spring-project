f8e79ed3f4e0af6d750f481508d4d317a631cdbe
ianklebold <ianstgo@gmail.com>
2021-07-22 14:06:39 -0300

    Listado de clientes en un template y en Json para secretaria

--- src/main/java/com/inmobiliaria/domus/controller/ClienteController.java
+++ src/main/java/com/inmobiliaria/domus/controller/ClienteController.java
@@ -3,13 +3,15 @@ import com.inmobiliaria.domus.services.DomusService;
 import com.inmobiliaria.domus.entity.Cliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView; 
+import org.springframework.ui.Model;
 import java.util.*;
 
 /**
@@ -17,31 +19,57 @@ import java.util.*;
  * 
  * RestController ejecuta el metodo del servicio, lo mapea en Json 
  * Y lo muestra en la pagina que indique RequestMapping
+ * 
+ *La declaración de anotación @Controller se usa para definir un 
+ *controlador SpringMVC en la clase para procesar la solicitud 
+ *enviada por el front-end.
+ *Cuando el método de la clase devuelve el resultado de String, 
+ *devolverá una vista (página), que es analizada por el analizador 
+ *de vistas SpringMVC y devuelta al front end.
+ * 
+ *La declaracion @RestController se usa solo si todos los metodos
+ *Van a devolver un Json. Si no todos los metodos devolveran Json
+ *Podemos indicar que será un Controller y en el metodo espcifico
+ *dejar un @ResponseBody
+ * 
+ * 
  */
-@RestController
+@Controller
 @RequestMapping("/clientes") //Indica en que pagina se va a ejecutar una peticion
 public class ClienteController {
     @Autowired
     DomusService domusService; 
 
+    @ResponseBody
     @GetMapping("/{apellido}") //Me lo mapea en Json
     public ArrayList<Cliente> obtenerCliente(@PathVariable ("apellido") String apellido ){
         return domusService.obtenerCliente();
     }
 
+    @ResponseBody
     @GetMapping("/orderDescId")
     public ArrayList<Cliente> obtenerOrdenadoDescClienteId(){
         return domusService.obtenerOrdenadoDescClienteId();
     }
 
-    @GetMapping("/orderAscId")
-    public ArrayList<Cliente> obtenerOrdenadoAscClienteId(){
-        return domusService.obtenerOrdenadoAscClienteId();
+    @RequestMapping("/orderAscId")
+    public String obtenerOrdenadoAscClienteId(Model model){
+        model.addAttribute("c",mostrarDatosAscId());
+        return "listaClientes";
+    }
+
+    @RequestMapping("/hola")
+    public String hola(Model modelo) {
+      
+      
+      modelo.addAttribute("mensaje","hola desde thymeleaf");
+      return "prueba/hola";
     }
 
 
-    
-
+    private ArrayList<Cliente> mostrarDatosAscId(){
+        return domusService.obtenerOrdenadoAscClienteId();
+    }
 
     @PostMapping()
     public Cliente guardarUsuario(@RequestBody Cliente clienteNuevo){
--- src/main/java/com/inmobiliaria/domus/entity/Cliente.java
+++ src/main/java/com/inmobiliaria/domus/entity/Cliente.java
@@ -13,6 +13,7 @@ public class Cliente {
 
     private String nombre;
     private String apellido;
+    
     private String email;
     private String telefono; 
 
